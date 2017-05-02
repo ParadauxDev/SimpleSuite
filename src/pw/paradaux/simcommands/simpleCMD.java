9c370e045121117bec295679e17cee3d54adb6e0
ParadauxDev <rian@rianscode.com>
2017-05-02 16:48:04 +0100

    v0.03pre3
    Fixes /simple again.

--- src/pw/paradaux/simcommands/simpleCMD.java
+++ src/pw/paradaux/simcommands/simpleCMD.java
@@ -1,13 +1,14 @@
 package pw.paradaux.simcommands;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import net.md_5.bungee.api.ChatColor;
 import pw.paradaux.simapi.prefix;
 
-public class simpleCMD {
+public class simpleCMD implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player p = (Player) sender;
 		if (!(sender instanceof Player)) {
--- src/pw/paradaux/simsuite/SimpleSuite.java
+++ src/pw/paradaux/simsuite/SimpleSuite.java
@@ -3,6 +3,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import pw.paradaux.simcommands.clearChat;
 import pw.paradaux.simcommands.gamemodeCMD;
+import pw.paradaux.simcommands.simpleCMD;
 public class SimpleSuite extends JavaPlugin {
 	public void onEnable() {
 		//Gamemode added v0.01
@@ -20,6 +21,8 @@ public class SimpleSuite extends JavaPlugin {
 		this.getCommand("clearchat").setExecutor(new clearChat());
 		this.getCommand("clearc").setExecutor(new clearChat());
 		this.getCommand("cc").setExecutor(new clearChat());
+		//Simple Main Commands added v0.03
+		this.getCommand("simple").setExecutor(new simpleCMD());
 	}
 	public void onDisable() {
 		
