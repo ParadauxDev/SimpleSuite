4af4ab1267223a085c44f881783b61d7c524f0b6
ParadauxDev <rian@rianscode.com>
2017-05-03 18:19:10 +0100

    Fixes chatHandler error.

--- src/pw/paradaux/simapi/chatHandler.java
+++ src/pw/paradaux/simapi/chatHandler.java
@@ -1,6 +1,7 @@
 package pw.paradaux.simapi;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
@@ -9,19 +10,25 @@ public class chatHandler implements Listener {
 	public static void clearChat(int amount, Player player) {
 		while (amount > 0) {
 			player.sendMessage("");
-			amount = amount - 1;
 		}
 	}
+	@EventHandler
 	public void onChat(AsyncPlayerChatEvent event) {
-		// if chat is muted don't let the player talk
 		if (chatHandler.getChatStatus() == true) {
 			event.setCancelled(true);
+			//System.out.println("[Simple Debug] event.setCancelled(true); called");
+		}
+		else {
+			event.setCancelled(false);
+			//System.out.println("[Simple Debug] event.setCancelled(false); called");
 		}
 	}
     public static void setChatMuted(boolean value) {
-        chatMuted = value;
+    	//System.out.println("[Simple Debug] setChatMuted() successfully called");
+    	chatMuted = value;
     }
     public static boolean getChatStatus() {
+    	//System.out.println("[Simple Debug] getChatStatus() successfully called");
         return chatMuted;
     }
 }
